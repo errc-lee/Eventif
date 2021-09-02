84132bf008853f80e951ed5cfbd38d6b131e8a98
Lindsay Baird <lindsay.a.baird@gmail.com>
2021-09-02 16:26:46 -0700

    cookie stuff Co-Authored By: Tre <hultzentre@gmail.com>

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -4,21 +4,29 @@ const cookieParser = require('cookie-parser');
 
 const cookieController = {};
 
+// Setting a cookie on the user's browser
 cookieController.setCookie = async (req, res, next) => {
   try {
+    const uniqueNum = Math.round(Math.random() * 100);
+    res.cookie('session', uniqueNum);
     return next();
-  }
-  catch (err) {
-    return next({log: 'error with cookieController.createCookie'});
+  } catch (err) {
+    return next({
+      log: `error with cookieController.createCookie ERROR: ${err}`,
+      message: { err: 'error setting cookies' },
+    });
   }
 };
 
+// Setting the session id to the aforementioned cookie's value
 cookieController.setSSIDCookie = async (req, res, next) => {
   try {
     return next();
-  }
-  catch (err) {
-    return next({log: 'error with cookieController.setSSIDCookie'});
+  } catch (err) {
+    return next({
+      log: `error with cookieController.setSSIDCookie ERROR: ${err}`,
+      message: { err: 'error setting SSID cookie' },
+    });
   }
 };
 
