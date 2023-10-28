8c6a8dfa15f8f6b90aaf6e0df825fce1e2de3f42
errc-lee <77417178+errc-lee@users.noreply.github.com>
2023-10-27 17:20:47 -0700

    vercel

--- server/index.js
+++ server/index.js
@@ -35,4 +35,4 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-module.exports = app.listen(PORT, () => { console.log(`Listening on port ${PORT}...`); });
+module.exports = app.listen(PORT, () => { console.log(`[INFO] Listening on port ${PORT}...`); });
